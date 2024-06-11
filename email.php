977d72a67ad5fee6877d8d1d12697a6e1d14c5ec
riyakatiyar13 <riyakatiyar72@gmail.com>
2024-06-11 20:44:24 +0530

    success

--- email.php
+++ email.php
@@ -1,40 +1,54 @@
 <?php
-if(isset($_POST['submit'])){
-  $name = $_POST['name']; //getting customer name
-  $email = $_POST['email']; //getting customer email
-  $message = $_POST['message']; //getting customer message
+if (isset($_POST['submit'])) {
+    $config = include('config.php'); // Include configuration file
 
-  $html="<table><tr><td>Name</td><td>$name</td></tr><tr><td>Email</td><td>$email</td></tr><tr><td>Message</td><td>$message</td></tr></table>";
+    $name = htmlspecialchars($_POST['name']); // Getting and sanitizing customer name
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL); // Getting and sanitizing customer email
+    $message = htmlspecialchars($_POST['message']); // Getting and sanitizing customer message
 
-  include('php/smtp/PHPMailerAutoload.php');
-  $mail = new PHPMailer(true);
-  $mail->isSMTP();
-    $mail->Host = 'smtp.gmail.com'; // SMTP server
-    $mail->SMTPAuth = true;
-    $mail->Username = 'riyakatiyar18@gmail.com'; // SMTP username
-    $mail->Password = ' ykwoxtlzdhmozzpt'; // SMTP password
-    $mail->SMTPSecure = 'tls';
-    $mail->Port = 587;
+    $html = "<table>
+                <tr><td>Name</td><td>{$name}</td></tr>
+                <tr><td>Email</td><td>{$email}</td></tr>
+                <tr><td>Message</td><td>{$message}</td></tr>
+             </table>";
 
-    //Recipients
-    $mail->setFrom("riyakatiyar18@gmail.com");
-    $mail->addAddress("riyakatiyar18@gmail.com");
+    include('php/smtp/PHPMailerAutoload.php');
+    $mail = new PHPMailer(true);
 
-    // Content
-    $mail->isHTML(true);
-    $mail->Subject = "New Query";
-    $mail->Body    = $html;
-    $mail->SMTPOptions=array('ssl'=>array(
-      'verify_peer'=>false,
-      'verify_peer_name'=>false,
-      'allow_self_signed'=>false
-    ));
+    try {
+        // Server settings
+        $mail->isSMTP();
+        $mail->Host = $config['SMTP_HOST'];
+        $mail->SMTPAuth = true;
+        $mail->Username = $config['SMTP_USERNAME'];
+        $mail->Password = $config['SMTP_PASSWORD'];
+        $mail->SMTPSecure = 'tls';
+        $mail->Port = $config['SMTP_PORT'];
 
-if($mail->send()){
-    // echo "success";
-  header("location:thankyou.html");
-}else{
-  echo "Error";
-}
+        // Recipients
+        $mail->setFrom($config['SMTP_FROM_EMAIL']);
+        $mail->addAddress($config['SMTP_TO_EMAIL']);
+
+        // Content
+        $mail->isHTML(true);
+        $mail->Subject = 'New Query';
+        $mail->Body = $html;
+        $mail->SMTPOptions = array(
+            'ssl' => array(
+                'verify_peer' => false,
+                'verify_peer_name' => false,
+                'allow_self_signed' => false
+            )
+        );
+
+        if ($mail->send()) {
+            header('Location: thankyou.html');
+            exit();
+        } else {
+            echo 'Error sending email.';
+        }
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
 }
 ?>
